3e0e2d52b2fd4dc16bc8528a0ee87a51ac069cef
StoyanKyopchev <stoyank43@gmail.com>
2024-01-25 18:42:40 +0200

    Created a database in MongoDB Atlas and connected the project to it.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import "dotenv/config";
 
-const app = express();
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
