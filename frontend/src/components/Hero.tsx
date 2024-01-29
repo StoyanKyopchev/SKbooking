eef7cbce93d8c03ae7814daf9622c57ead445ef0
StoyanKyopchev <stoyank43@gmail.com>
2024-01-29 13:43:09 +0200

    Created the Hero component.

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <>
+      <div className="bg-sky-800 pb-16">
+        <div className="container mx-auto flex flex-col gap-2">
+          <h1 className="text-5xl text-white font-bold">
+            Enjoy the finest stays
+          </h1>
+          <p className="text-2xl text-white">
+            Find low prices on hotels for your dream vacation...
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
