5e4fea98a0b6e592f350810612bf87eb0884d8c6
StoyanKyopchev <stoyank43@gmail.com>
2024-01-29 13:02:01 +0200

    Installed react router, created 2 routes and replaced <a> tags with the Link component.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/images/Logo1.svg";
 
 const Header = () => {
   return (
-    <div className="bg-sky-700 py-6">
+    <div className="bg-sky-800 py-6">
       <div className="container flex justify-between mx-auto">
-        <a>
+        <Link to="/">
           <img src={logo} alt="Home page button" />
-        </a>
+        </Link>
         <span className="flex space-x-2">
-          <a className="flex justify-center items-center bg-white text-sky-500 px-3 font-bold rounded hover:bg-gray-100">
+          <Link
+            to="/sign-in"
+            className="flex justify-center items-center bg-white text-sky-600 px-3 font-bold rounded hover:bg-gray-100"
+          >
             Sign In
-          </a>
+          </Link>
         </span>
       </div>
     </div>
