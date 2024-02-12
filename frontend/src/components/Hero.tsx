b06ff170d42d70ad3032b34f63c6fe6d7203fbf4
StoyanKyopchev <stoyank43@gmail.com>
2024-02-12 13:20:10 +0200

    Improved the responsiveness of the Hero component.

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -3,15 +3,15 @@ const Hero = () => {
     <>
       <div className="bg-sky-800 pb-2">
         <div className="container max-sm:px-2.5 mx-auto flex flex-col gap-2">
-          <h1 className="text-5xl text-white font-bold">
+          <h1 className="text-3xl md:text-5xl text-white font-bold">
             Enjoy the finest stays
           </h1>
-          <p className="text-2xl text-white">
+          <p className="text-xl md:text-2xl text-white">
             Find low prices on hotels for your dream vacation...
           </p>
         </div>
       </div>
-      <div className="bg-hero-pattern h-20"></div>
+      <div className="bg-hero-pattern h-12 md:h-20"></div>
     </>
   );
 };
