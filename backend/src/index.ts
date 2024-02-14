3961bfafdb808ef3cf1156e00f99f01b4808b36d
StoyanKyopchev <stoyank43@gmail.com>
2024-02-14 11:56:30 +0200

    Initial setup of Cloudinary on the server.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,8 +5,15 @@ import "dotenv/config";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(cookieParser());
