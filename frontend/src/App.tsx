da74e4bd39ba494612e008bfb34d951305fa7549
StoyanKyopchev <stoyank43@gmail.com>
2024-03-07 15:25:01 +0200

    Created a new route on the frontend for the My Hotels page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import ScrollToHashElement from "./components/ScrollToHashElement";
 import AddHotel from "./pages/AddHotel";
 import { ManageHotelFormContextProvider } from "./contexts/ManageHotelFormContext";
+import MyHotels from "./pages/MyHotels";
 
 function App() {
   const authContext = useContext(AuthContext);
@@ -48,6 +49,14 @@ function App() {
                   element={
                     <Layout>
                       <AddHotel />
+                    </Layout>
+                  }
+                />
+                <Route
+                  path="/my-hotels"
+                  element={
+                    <Layout>
+                      <MyHotels />
                     </Layout>
                   }
                 />
