ed3b17a50c9e1f5a875200b80b165cfbf82d1765
StoyanKyopchev <stoyank43@gmail.com>
2024-03-11 11:28:51 +0200

    Created a new endpoint on the server that finds a particular hotel in the DB and returns it to the client.

--- backend/src/routes/myHotels.ts
+++ backend/src/routes/myHotels.ts
@@ -66,10 +66,25 @@ router.get("/", verifyToken, async (req: Request, res: Response) => {
   try {
     const hotels = await Hotel.find({ userId: req.userId });
     res.json(hotels);
-  } catch {
+  } catch (error) {
     res
       .status(500)
       .json({ message: "An error occurred while loading the hotels" });
+  }
+});
+
+router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+  const id = req.params.id.toString();
+  try {
+    const hotel = await Hotel.find({
+      _id: id,
+      userId: req.userId,
+    });
+    res.json(hotel);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred while loading the selected hotel" });
   }
 });
 
