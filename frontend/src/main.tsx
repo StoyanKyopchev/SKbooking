861e75b45ff1fc6570dfe9d4d6fc62d8ef949c8e
StoyanKyopchev <stoyank43@gmail.com>
2024-03-13 12:37:03 +0200

    Created a search context on the client side that will store the different search parameters.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext.tsx";
+import { SearchContextProvider } from "./contexts/SearchContext.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
