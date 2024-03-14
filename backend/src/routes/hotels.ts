17ad86035d56a3f10f090878170ba8f4f182059a
StoyanKyopchev <stoyank43@gmail.com>
2024-03-14 19:11:12 +0200

    Created a new function on the server for the search endpoint. It now correctly finds hotels in the DB based on the provided search parameters from the client.

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -15,15 +15,17 @@ export type HotelSearchResponse = {
 
 router.get("/search", async (req: Request, res: Response) => {
   try {
+    const query = constructSearchQuery(req.query);
+
     const pageSize = 5;
     const pageNumber = parseInt(
       req.query.page ? req.query.page.toString() : "1"
     );
     const hotelsToSkip = (pageNumber - 1) * pageSize;
 
-    const hotels = await Hotel.find().skip(hotelsToSkip).limit(pageSize);
+    const hotels = await Hotel.find(query).skip(hotelsToSkip).limit(pageSize);
 
-    const total = await Hotel.countDocuments();
+    const total = await Hotel.countDocuments(query);
 
     const response: HotelSearchResponse = {
       data: hotels,
@@ -40,5 +42,30 @@ router.get("/search", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Oops, something went wrong." });
   }
 });
+
+const constructSearchQuery = (queryParams: any) => {
+  let constructedQuery: any = {};
+
+  if (queryParams.destination) {
+    constructedQuery.$or = [
+      { city: new RegExp(queryParams.destination, "i") },
+      { country: new RegExp(queryParams.destination, "i") },
+    ];
+  }
+
+  if (queryParams.adultCount) {
+    constructedQuery.adultCount = {
+      $gte: parseInt(queryParams.adultCount),
+    };
+  }
+
+  if (queryParams.childCount) {
+    constructedQuery.childCount = {
+      $gte: parseInt(queryParams.childCount),
+    };
+  }
+
+  return constructedQuery;
+};
 
 export default router;
