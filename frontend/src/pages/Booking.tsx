83f3863e961484e79d1c8a8a5e73381cc27f4107
StoyanKyopchev <stoyank43@gmail.com>
2024-03-27 15:39:18 +0200

    Created a function in the Booking page which calculates the number of nights that will be booked based on the check in and check out dates. Also added the functionality to fetch the selected hotel by ID.

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,15 +1,40 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { UserType } from "../../../backend/src/models/user";
+import { useSearchContext } from "../contexts/SearchContext";
+import { HotelType } from "../../../backend/src/models/hotel";
 import BookingForm from "../forms/BookingForm/BookingForm";
 
 const SERVER_BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
 
 const Booking = () => {
+  const search = useSearchContext();
+  const { hotelId } = useParams();
   const [currentUser, setCurrentUser] = useState<UserType>();
   const [error, setError] = useState<string>("");
+  const [hotelData, setHotelData] = useState<HotelType>();
+  const [numberOfNights, setNumberOfNights] = useState<number>(0);
+
+  async function findHotelById(hotelId: string) {
+    try {
+      setError("");
+      const response = await fetch(`${SERVER_BASE_URL}/api/hotels/${hotelId}`);
+      if (!response.ok) {
+        throw new Error("An error occurred while trying to display this hotel");
+      }
+
+      const data: HotelType = await response.json();
+      setHotelData(data);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    }
+  }
 
   async function fetchCurrentUser() {
     try {
+      setError("");
       const response = await fetch(`${SERVER_BASE_URL}/api/users/me`, {
         credentials: "include",
       });
@@ -29,6 +54,21 @@ const Booking = () => {
   useEffect(() => {
     fetchCurrentUser();
   }, []);
+
+  useEffect(() => {
+    if (hotelId) {
+      findHotelById(hotelId);
+    }
+  }, [hotelId]);
+
+  useEffect(() => {
+    if (search.checkIn && search.checkOut) {
+      const nights =
+        Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
+        (1000 * 60 * 60 * 24);
+      setNumberOfNights(Math.ceil(nights));
+    }
+  }, [search.checkIn, search.checkOut]);
 
   return (
     <div className="grid md:grid-cols-[1fr_2fr]">
