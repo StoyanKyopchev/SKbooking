4a3a0d324725c24db01ac9b883ddbbea974b276d
StoyanKyopchev <stoyank43@gmail.com>
2024-03-28 15:07:01 +0200

    Created a new endpoint on the server which verifies the payment intent and creates a new booking.

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { HotelType } from "../models/hotel";
+import { BookingType, HotelType } from "../models/hotel";
 import Hotel from "../models/hotel";
 import { param, validationResult } from "express-validator";
 import Stripe from "stripe";
@@ -127,6 +127,60 @@ router.post(
       totalCost,
     };
     res.send(response);
+  }
+);
+
+router.post(
+  "/:hotelId/bookings",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    try {
+      const paymentIntentId = req.body.paymentIntentId;
+
+      const paymentIntent = await stripe.paymentIntents.retrieve(
+        paymentIntentId as string
+      );
+
+      if (!paymentIntent) {
+        return res.status(400).json({ message: "Payment intent not found" });
+      }
+
+      if (
+        paymentIntent.metadata.hotelId !== req.params.hotelId ||
+        paymentIntent.metadata.userId !== req.userId
+      ) {
+        return res.status(400).json({ message: "Payment intent mismatch" });
+      }
+
+      if (paymentIntent.status !== "succeeded") {
+        return res.status(400).json({
+          message: `Payment intent not succeeded. Status: ${paymentIntent.status}`,
+        });
+      }
+
+      const newBooking: BookingType = {
+        ...req.body,
+        userId: req.userId,
+      };
+
+      const hotel = await Hotel.findOneAndUpdate(
+        {
+          _id: req.params.hotelId,
+        },
+        {
+          $push: { bookings: newBooking },
+        }
+      );
+
+      if (!hotel) {
+        return res.status(400).json({ message: "Hotel not found" });
+      }
+
+      await hotel.save();
+      res.status(200).send();
+    } catch (error) {
+      res.status(500).json({ message: "Oops, something went wrong." });
+    }
   }
 );
 
