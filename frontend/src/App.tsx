d1adad0d27e5a4c9a8b975f35454549f6eaf49e6
StoyanKyopchev <stoyank43@gmail.com>
2024-04-03 16:12:35 +0300

    Created a new route for the my bookings page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 function App() {
   const authContext = useContext(AuthContext);
@@ -85,6 +86,14 @@ function App() {
                   element={
                     <Layout>
                       <MyHotels />
+                    </Layout>
+                  }
+                />
+                <Route
+                  path="/my-bookings"
+                  element={
+                    <Layout>
+                      <MyBookings />
                     </Layout>
                   }
                 />
